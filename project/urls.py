72ca4495e292bdb105433b03ebd503a6eddd837c
tuan anh <canhazn@gmail.com>
2020-08-21 14:50:57 +0700

    Pageing list project

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,6 @@ from project import views
 
 
 urlpatterns = [
-    path('du-an-noi-bat/', views.projectList, name="project-list"),
+    path('du-an-noi-bat/', views.ProjectList.as_view(), name="project-list"),
     path('du-an-noi-bat/<slug:slug>/', views.projectDetail, name="project-detail")
 ]
--- project/views.py
+++ project/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from project import models, serializers
+from django.core.paginator import Paginator
+
+
+class ProjectList(ListView):
+    model = models.Project
+    paginate_by = 12
+    template_name = 'project_list.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        # Add in a QuerySet of all the books
+        context['app_url'] = "project"
+        return context
 
 
 def projectList(request):
     queryset = models.Project.objects.all()
+    paginator = Paginator(queryset, 12)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     projectSerializer = serializers.ProjectSerializer(queryset, many=True)
     tagSerializer = serializers.CategorySerializer(
         models.Category.objects.all(), many=True)
@@ -12,7 +32,7 @@ def projectList(request):
         "app_url": "project",
         "projects": projectSerializer.data,
         "tags": tagSerializer.data,
-        "message": "hello"
+        "page_obj": page_obj,
     }
     return render(request, 'project_list.html', context)
 
